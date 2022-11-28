db68ef7c29650c7424c156040bff24da2aed8c68
pimuk <pimukov.pavel@gmail.com>
2022-11-28 23:40:11 +0400

    clear script

---
+++ src/main/resources/sql/delete/clear_database.sql
@@ -0,0 +1,9 @@
+drop table admins;
+drop table promocode;
+drop table review;
+drop table element;
+drop table product;
+drop table section;
+drop table orders;
+drop table client;
+drop table category;
